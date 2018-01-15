41711a950052148a09e8ae12bf4ba268b8bc69cb
Jibbajabba <jibbajabbafication@gmail.com>
2018-01-15 00:02:55 +0000

    Fixed most things, added wheel colour order

--- RGB_Controller.ino
+++ RGB_Controller.ino
@@ -20,7 +20,7 @@ class LED {
     public:
         // Define patterns and orders
         enum pattern { FLASH, FADE };
-        enum order { FORWARD, BACKWARD, RANDOM };
+        enum order { FORWARD, BACKWARD, WHEEL, RANDOM };
         
         // ---------------------------- VARIABLES ----------------------------
 
@@ -44,10 +44,14 @@ class LED {
         byte TotalSteps = 17;
         byte currentStep = 0;
 
+        byte red;
+        byte green;
+        byte blue;
+
         float dutyCycle;
         bool onState;
 
-        // ---------------------------- FUNCTIONS ----------------------------
+        // ---------------------------- PATTERN FUNCTIONS ----------------------------
 
         void setPattern(pattern inputPattern, order inputOrder, byte inputDenom = 4, float inputDuty = 0.25) {
             // Variables common to all patterns
@@ -67,86 +71,6 @@ class LED {
                     break;
             }
         }
-
-        // ---------------------------- BPM FUNCTIONS ----------------------------
-
-        byte UpdateBPM(int change) {
-            if ((BPM - change >= 0) && (BPM + change <= 255)) {
-                BPM += change;
-                updateTiming();
-            }
-            return BPM;
-        }
-
-        void updateTiming() {
-            TotalInterval = calcMS(BeatDenom);
-            if (ActivePattern == FLASH) {
-                onTime = TotalInterval*dutyCycle;
-                offTime = TotalInterval - onTime;
-            }
-        }
-
-        float calcMS(byte denom) {
-            return 60000/(BPM*denom);
-        }
-
-        // ---------------------------- COLOUR FUNCTIONS ----------------------------
-
-        // Returns the Red component of a 32-bit color
-        uint8_t Red(uint32_t color)
-        {
-            return (color >> 16) & 0xFF;
-        }
-
-        // Returns the Green component of a 32-bit color
-        uint8_t Green(uint32_t color)
-        {
-            return (color >> 8) & 0xFF;
-        }
-
-        // Returns the Blue component of a 32-bit color
-        uint8_t Blue(uint32_t color)
-        {
-            return color & 0xFF;
-        }
-
-        uint32_t getNextColour() {
-            switch(ColourOrder) {
-                // Select the next colour number
-                case FORWARD:
-                    // (R->G->B->R...)
-                    if (currentColour > 0x0000FF) {
-                        return currentColour >> 8;
-                    }
-                    else {
-                        return 0xFF0000;
-                    }
-                case BACKWARD:
-                    // (B->G->R->B...)
-                    if (currentColour < 0xFF0000) {
-                        return currentColour << 8;
-                    }
-                    else {
-                        return 0x0000FF;
-                    }
-                case RANDOM:
-                    // Random colour;
-                    return (random(255) << 16 | random(255) << 8 | random(255));
-            }
-        }
-
-        uint32_t Colour(uint8_t r, uint8_t g, uint8_t b) {
-            return ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
-        }
-
-        void setColour(uint32_t inputColour) {
-            analogWrite(R_PIN, Red(inputColour));
-            analogWrite(G_PIN, Green(inputColour));
-            analogWrite(B_PIN, Blue(inputColour));
-            return;
-        }
-
-        // ---------------------------- PATTERN FUNCTIONS ----------------------------
 
         void Update() {
             if((millis() - lastUpdate) > Interval) {
@@ -200,15 +124,122 @@ class LED {
             // Calculate linear interpolation between Color1 and Color2
             // Optimise order of operations to minimize truncation error
             if (currentStep >= TotalSteps) {
+                currentColour = nextColour;
                 nextColour = getNextColour();
                 currentStep = 0;
             }
-            uint8_t red = ((Red(currentColour) * (TotalSteps - currentStep)) + (Red(nextColour) * currentStep)) / TotalSteps;
-            uint8_t green = ((Green(currentColour) * (TotalSteps - currentStep)) + (Green(nextColour) * currentStep)) / TotalSteps;
-            uint8_t blue = ((Blue(currentColour) * (TotalSteps - currentStep)) + (Blue(nextColour) * currentStep)) / TotalSteps;
-            
+
+            red = ((Red(currentColour) * (TotalSteps - currentStep)) + (Red(nextColour) * currentStep)) / TotalSteps;
+            green = ((Green(currentColour) * (TotalSteps - currentStep)) + (Green(nextColour) * currentStep)) / TotalSteps;
+            blue = ((Blue(currentColour) * (TotalSteps - currentStep)) + (Blue(nextColour) * currentStep)) / TotalSteps;
+
             setColour(Colour(red, green, blue));
             ++currentStep;
+        }
+
+        // ---------------------------- BPM FUNCTIONS ----------------------------
+
+        byte UpdateBPM(int change) {
+            if ((BPM + change >= 0) && (BPM + change <= 255)) {
+                BPM += change;
+                updateTiming();
+            }
+            return BPM;
+        }
+
+        void updateTiming() {
+            TotalInterval = calcMS(BeatDenom);
+            switch (ActivePattern) {
+                case FLASH:
+                    onTime = TotalInterval*dutyCycle;
+                    offTime = TotalInterval - onTime;
+                    break;
+                case FADE:
+                    Interval = TotalInterval/TotalSteps;
+                    break;
+            }
+        }
+
+        float calcMS(byte denom) {
+            return 4*60000/(BPM*denom);
+        }
+
+        // ---------------------------- COLOUR FUNCTIONS ----------------------------
+
+        // Returns the Red component of a 32-bit color
+        uint8_t Red(uint32_t color)
+        {
+            return (color >> 16) & 0xFF;
+        }
+
+        // Returns the Green component of a 32-bit color
+        uint8_t Green(uint32_t color)
+        {
+            return (color >> 8) & 0xFF;
+        }
+
+        // Returns the Blue component of a 32-bit color
+        uint8_t Blue(uint32_t color)
+        {
+            return color & 0xFF;
+        }
+
+        uint32_t getNextColour() {
+            switch(ColourOrder) {
+                // Select the next colour number
+                case FORWARD:
+                    // (R->G->B->R...)
+                    if (currentColour > 0x0000FF) {
+                        return uint32_t(currentColour >> 8);
+                    }
+                    else {
+                        return uint32_t(0xFF0000);
+                    }
+                case BACKWARD:
+                    // (B->G->R->B...)
+                    if (currentColour < 0xFF0000) {
+                        return uint32_t(currentColour << 8);
+                    }
+                    else {
+                        return uint32_t(0x0000FF);
+                    }
+                case WHEEL:
+                    // Use a colour wheel to determine the next colour
+                    return Wheel(random(255));
+                case RANDOM:
+                    // Random colour;
+                    return Colour(random(255), random(255), random(255));
+            }
+        }
+
+        uint32_t Colour(uint8_t r, uint8_t g, uint8_t b) {
+            return ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
+        }
+
+        void setColour(uint32_t inputColour) {
+            analogWrite(R_PIN, Red(inputColour));
+            analogWrite(G_PIN, Green(inputColour));
+            analogWrite(B_PIN, Blue(inputColour));
+            return;
+        }
+
+        uint32_t Wheel(byte WheelPos)
+        {
+            WheelPos = 255 - WheelPos;
+            if(WheelPos < 85)
+            {
+                return Colour(255 - WheelPos * 3, 0, WheelPos * 3);
+            }
+            else if(WheelPos < 170)
+            {
+                WheelPos -= 85;
+                return Colour(0, WheelPos * 3, 255 - WheelPos * 3);
+            }
+            else
+            {
+                WheelPos -= 170;
+                return Colour(WheelPos * 3, 255 - WheelPos * 3, 0);
+            }
         }        
 };
 
@@ -241,6 +272,8 @@ void printBPM(int bpm) {
 LED rgb_lights;
 
 void setup() {
+    Serial.begin(9600);
+    Serial.println("My Sketch has started");
     pinMode(R_PIN, OUTPUT);
     pinMode(G_PIN, OUTPUT);
     pinMode(B_PIN, OUTPUT);
@@ -254,7 +287,8 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(DEC_PIN), decrementInterval, FALLING);
 
     // Start rgb_lights with a pattern
-    rgb_lights.setPattern(LED::FLASH, LED::FORWARD, 4, 0.25);
+    rgb_lights.setPattern(LED::FLASH, LED::WHEEL, 4, 0.25);
+    // rgb_lights.setPattern(LED::FADE, LED::FORWARD, 8, 0.25);
 
     // set up the LCD's number of columns and rows: 
     lcd.begin(16, 2);
@@ -263,6 +297,12 @@ void setup() {
 
 void loop() {
     rgb_lights.Update();
+    Serial.println(rgb_lights.red);
+    Serial.println(rgb_lights.green);
+    Serial.println(rgb_lights.blue);
+    Serial.println(rgb_lights.currentStep);
+    Serial.println(rgb_lights.currentColour);
+    Serial.println(rgb_lights.nextColour);
 }
 
 // ---------------------------- INTERRUPT FUNCTIONS ----------------------------

a00c661183410110d9ed4fb9f58dbca327775cd5
Jibbajabba <jibbajabbafication@gmail.com>
2018-01-30 23:32:04 +0000

    Updated non-addressable led to support midi

--- RGB_MIDI_Timing.ino
+++ RGB_MIDI_Timing.ino
@@ -160,6 +160,7 @@ class RGB_Colours {
         return colour & 0xFF;
     }
 
+    // Makes a 32 bit colour using three 8 bit RGB values
     uint32_t Colour(uint8_t r, uint8_t g, uint8_t b) {
         return ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
     }
@@ -183,13 +184,159 @@ class RGB_Colours {
         }
     }
 
-    uint32_t Complement(uint32_t ColourCode) {
-        return 0x00FFFFFF - ColourCode;
+    uint32_t Complement(uint32_t colour) {
+        return 0x00FFFFFF - colour;
     }
+
+    uint32_t ShiftRight(uint32_t colour) {
+        // Check if colour less than or equal to 0xFF (so blue values)
+        // Shift right if true, otherwise wrap around to red
+        return (colour <= 0x0000FF) ? colour << 16 : colour >> 8;
+    }
+
+    uint32_t ShiftLeft(uint32_t colour) {
+        // Check if colour greater than 0xFF00 (so red values)
+        // Shift left if true, otherwise wrap around to blue
+        return (colour > 0x00FF00) ? colour >> 16 : colour << 8;
+    }    
 };
 
 class LED_Strip : public RGB_Colours {
 
+    // ---------------------------- PUBLIC ----------------------------
+
+    public:
+    
+    // Callback when pattern complete
+    void (*OnComplete)();
+
+    uint32_t Colour1, Colour2;
+
+    LED_Strip(uint8_t r_pin, uint8_t g_pin, uint8_t b_pin, void (*callback)()) {
+        _r_pin = r_pin;
+        _g_pin = g_pin;
+        _b_pin = b_pin;
+
+        OnComplete = callback;
+    }
+
+    void Update() {
+
+        // Update which tick and beat currently on
+
+        // Check if Tick is overflowing
+        if (Tick == MaxTicks - 1) {
+            Tick = 0;
+            
+            if (OnComplete != NULL) {
+                OnComplete();
+            }
+
+            // Check if Beat is overflowing
+            if (Beat == MaxBeats - 1) {
+                Beat = 0;
+            }
+            else {
+                ++Beat;
+            }
+        }
+        else {
+            ++Tick;
+        }
+
+        switch(ActivePattern) {
+            case FLASH:
+                FlashUpdate();
+                break;       
+            case FADE:
+                FadeUpdate();
+                break;
+            case STROBE:
+                StrobeUpdate();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // ---------------------------- PATTERN FUNCTIONS ----------------------------
+
+    void Flash(uint32_t initColour1 = 0xFF0000) {
+        ActivePattern = FLASH;
+        Colour1 = initColour1;
+    }
+
+    void Fade(uint32_t initColour1 = 0xFF0000) {
+        ActivePattern = FADE;
+        Colour1 = initColour1;
+    }
+
+    void Strobe(uint32_t initColour1 = 0xFFFFFF) {
+        ActivePattern = STROBE;
+        Colour1 = initColour1;
+    }
+
+    // ---------------------------- PRIVATE ----------------------------
+
+    private:
+
+    // Defining patterns to easily pass them to functions
+    enum pattern { FLASH, FADE, STROBE };
+
+    static const uint8_t MaxTicks = 24;
+    static const uint16_t MaxBeats = 256;
+
+    pattern ActivePattern;
+    uint8_t _r_pin, _g_pin, _b_pin;
+
+    uint8_t Tick = 0;
+    uint16_t Beat = 0;
+
+    // ---------------------------- PATTERN UPDATE FUNCTIONS ----------------------------
+
+    void FlashUpdate() {
+
+        // Check if on beat and turn on light
+        if (Tick == 0) {
+            ColourSet(Colour1);
+        }
+        // Turn off after short delay
+        else if (Tick == 4) {
+            ColourSet(0);
+        }
+
+    }
+
+    void FadeUpdate() {
+
+        // Calculate linear interpolation between colour1 and colour2
+        // Optimise order of operations to minimize truncation error
+        uint8_t red = ((Red(Colour1) * (MaxTicks - 1 - Tick)) + (Red(Colour2) * Tick)) / MaxTicks;
+        uint8_t green = ((Green(Colour1) * (MaxTicks - 1 - Tick)) + (Green(Colour2) * Tick)) / MaxTicks;
+        uint8_t blue = ((Blue(Colour1) * (MaxTicks - 1 - Tick)) + (Blue(Colour2) * Tick)) / MaxTicks;
+
+        ColourSet(Colour(red, green, blue));
+    }
+
+    void StrobeUpdate() {
+        
+        // Check if on beat and turn on light
+        if (Tick == 0) {
+            ColourSet(Colour1);
+        }
+        // Turn off after short delay
+        else if (Tick == 2) {
+            ColourSet(0);
+        }
+    }    
+
+    // ---------------------------- UTILITY FUNCTIONS ----------------------------
+
+    void ColourSet(uint32_t colour) {
+        analogWrite(_r_pin, Red(colour));
+        analogWrite(_g_pin, Green(colour));
+        analogWrite(_b_pin, Blue(colour));
+    }
 };
 
 class LED_Addr : public RGB_Colours {
@@ -197,7 +344,10 @@ class LED_Addr : public RGB_Colours {
 };
 
 // Initialise MIDI_Sync object
-MIDI_Sync midi_sync(&Serial1, 128, 3000, &tick, &beat, &reset);
+MIDI_Sync midiSync(&Serial1, 128, 3000, &tick, &beat, &reset);
+
+//Initialise LED_Strip object
+LED_Strip strip1(R_PIN, G_PIN, B_PIN, &complete);
 
 // Initialise lcd display with corresponding pins
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
@@ -206,13 +356,14 @@ byte state = 0;
 unsigned long prev_time;
 
 void setup() {
+    strip1.Flash();
     lcd.begin(16, 2);
     lcd.clear();
     lcd.print("Setup!");
 }
 
 void loop() {
-    midi_sync.Update();
+    midiSync.Update();
 
     if (millis() - prev_time > 100 && state) {
         analogWrite(31, 0);
@@ -221,13 +372,13 @@ void loop() {
 }
 
 void tick() {
-
+    strip1.Update();
 }
 
 void beat() {
     lcd.clear();
     lcd.setCursor(0,1);
-    lcd.print(midi_sync.GetBPM());
+    lcd.print(midiSync.GetBPM());
     lcd.print(" BPM");
     analogWrite(31, 255);
     state = 1;
@@ -237,4 +388,8 @@ void beat() {
 void reset() {
     lcd.clear();
     lcd.print("No clock signal!");
+}
+
+void complete() {
+    strip1.Colour1 = strip1.ShiftRight(strip1.Colour1);
 }
